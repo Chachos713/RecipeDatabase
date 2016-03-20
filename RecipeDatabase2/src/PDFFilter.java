02673eba21b1b480a1336e505d34112ad1cc30e2
Kyle <kdiller99@gmail.com>
2016-03-20 18:34:27 -0400

    Started to comment the code for easy to read use.

--- RecipeDatabase2/src/PDFFilter.java
+++ RecipeDatabase2/src/PDFFilter.java
@@ -2,25 +2,39 @@ import java.io.File;
 
 import javax.swing.filechooser.FileFilter;
 
-
+/**
+ * A simple class that extends file filter to save to a pdf file (.rtf for now)
+ * 
+ * @author Kyle Diller
+ *
+ */
 public class PDFFilter extends FileFilter {
-private final String[] okFileExt = new String[] {".rtf"};
-    
-    public boolean accept(File file) {
-        if (file.isDirectory()) {
-            return true;
-        }
-        
-        for (String ext : okFileExt) {
-            if (file.getName().toLowerCase().endsWith(ext)) {
-                return true;
-            }
-        }
-        
-        return false;
-    }
-    
-    public String getDescription() {
-        return ".rtf";
-    }
+	/**
+	 * The array of acceptable file extensions
+	 */
+	private final String[] okFileExt = new String[] { ".rtf" };
+
+	/* (non-Javadoc)
+	 * @see javax.swing.filechooser.FileFilter#accept(java.io.File)
+	 */
+	public boolean accept(File file) {
+		if (file.isDirectory()) {
+			return true;
+		}
+
+		for (String ext : okFileExt) {
+			if (file.getName().toLowerCase().endsWith(ext)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.swing.filechooser.FileFilter#getDescription()
+	 */
+	public String getDescription() {
+		return ".rtf";
+	}
 }
--- RecipeDatabase2/src/TXTFilter.java
+++ RecipeDatabase2/src/TXTFilter.java
@@ -2,9 +2,24 @@ import java.io.File;
 
 import javax.swing.filechooser.FileFilter;
 
+/**
+ * A simple class that is used to filter files based on a .txt extension. Used
+ * when reading and writing a database.
+ * 
+ * @author Kyle Diller
+ *
+ */
 public class TXTFilter extends FileFilter {
+	/**
+	 * The array of acceptable extensions
+	 */
 	private final String[] okFileExt = new String[] { ".txt" };
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.swing.filechooser.FileFilter#accept(java.io.File)
+	 */
 	public boolean accept(File file) {
 		if (file.isDirectory()) {
 			return true;
@@ -19,6 +34,11 @@ public class TXTFilter extends FileFilter {
 		return false;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.swing.filechooser.FileFilter#getDescription()
+	 */
 	public String getDescription() {
 		return ".txt";
 	}
